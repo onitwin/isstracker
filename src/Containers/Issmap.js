3b53784179f1e6a8168d46e3bff24b140f60c617
onitwin <onitwin@gmail.com>
2020-09-23 12:07:13 +0100

    add popup data

--- src/Containers/Issmap.js
+++ src/Containers/Issmap.js
@@ -1,21 +1,23 @@
 import React ,{useState,useEffect} from 'react'
-import { Map,Marker,TileLayer,Polyline } from 'react-leaflet';
+import { Map,Marker,TileLayer,Polyline,Popup } from 'react-leaflet';
 import {Icon} from "leaflet"
 
 
 
 const Issmap=()=>{
-  const centre=[40.7128,3.7492]
 
   const [data,setData]=useState([]);
   const [currentPosition,setCurrentPosition]=useState([10.0000,1.0000])
+  const [allData,setAllData]=useState([]);
+
+  const centre=[40.7128,3.7492];
 
 
 
   useEffect(()=>{
     setTimeout(()=>{
       fetchFunction()
-  },5000)
+  },1000)
 },[data])
 
 
@@ -25,6 +27,7 @@ const Issmap=()=>{
     .then(data2=>{
       const coords=[data2.latitude,data2.longitude]
     setData([...data,coords])
+    setAllData(data2)
     console.table(data)
     if(data){
       setCurrentPosition(coords)
@@ -44,14 +47,14 @@ const Issmap=()=>{
 
 return(
 <div className='mapdiv'>
-<h3 className="infoHeader">Data polls current position every 5 seconds- two positions required so 10 seconds before line appears</h3>
-<Map  className='darkMap' center={centre} zoom={2} attributionControl={false} zoomControl={false} >
+<h3 className="infoHeader">Data polls current position once per second</h3>
+<Map  className='darkMap' center={centre} zoom={2} attributionControl={false} zoomControl={false} alt="map">
     <TileLayer
-    url='https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png'
+    url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     />
       <Polyline color="red" positions={data}/>
-      <Marker position={currentPosition} icon={iss}/>
+      <Marker position={currentPosition} icon={iss}><Popup><p className='popup'><strong>Altitude:</strong>{allData.altitude}</p><p className='popup'><strong>Velocity:</strong>{allData.velocity}</p><p className='popup'><strong>Latitude:</strong>{allData.latitude}</p> <p className='popup'><strong>Longtitude:</strong>{allData.longitude}</p></Popup></Marker>
 
 
 
